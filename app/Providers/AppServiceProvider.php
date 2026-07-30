c3a1c8c5173ee5c51692c2894ba9e804f1ae195b
Ichwan027 <ichwanm888@gmail.com>
2026-07-30 12:45:35 +0700

    paginate clear

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Core\Repositories\NilaiRepository;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Paginator::useBootstrapFive(); // tambahkan baris ini
     }
 }
